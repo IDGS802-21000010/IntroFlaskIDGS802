048bf545bd0dadba1b17ad27f774a918e4e95845
IDSGS802-21000010 <gaboguerrero2024@gmail.com>
2024-02-19 20:52:53 -0600

    Validaciones

--- forms.py
+++ forms.py
@@ -1,10 +1,15 @@
 from wtforms import Form
-from wtforms import SearchField, TextAreaField, RadioField, StringField, SelectField
+from wtforms import SearchField, TextAreaField, RadioField, StringField, SelectField, IntegerField
 from wtforms import EmailField
+from wtforms import validators
 
 class UserForm(Form):
-    nombre=StringField("nombre")
-    email=EmailField("email")
-    apaterno=StringField("apaterno")
+    nombre=StringField('nombre',[validators.DataRequired(message='El campo es requerido'),
+                                 validators.length(min=4, max=10, message='Ingresa nombre valido')
+    ])
+    email=EmailField('email')
+    apaterno=StringField('apaterno')
+    edad=IntegerField('edad', [validators.number_range(min=1,max=20, message='valor no valido')])
+    correo=EmailField('correo',[validators.Email(message='Ingrese un correo valido')])
     materias=SelectField(choices=[('Español','Esp'),('Mat','Matematicas'),('Ingels', 'ING')])
     radios=RadioField('Curso',choices=[('1','1'),('2','2'),('3','3')])
